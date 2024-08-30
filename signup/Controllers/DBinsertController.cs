592055d29e7d52c086b26e37a33cab7bba77f7fb
Acer <Acer@DESKTOP-MF4J9K9>
2024-08-30 08:12:08 +0530

    Add project files.

---
+++ signup/Controllers/DBinsertController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using signup.Models;
+
+namespace signup.Controllers
+{
+    public class DBinsertController : Controller
+    {
+        AugDBEntities dbobj = new AugDBEntities();
+        // GET: DBinsert
+        public ActionResult Insert_pageload()
+        {
+            return View();
+        }
+        public ActionResult Insert_click(stinsert clsobj)
+        {
+            if (ModelState.IsValid)
+            {
+                dbobj.sp_stinsert(clsobj.name, clsobj.uname, clsobj.pwd, clsobj.cpwd);
+                clsobj.msg = "inserted";
+                return View("Insert_pageload", clsobj);
+            }
+            return View("Insert_pageload", clsobj);
+        }
+    }
+}
---
+++ signup/Models/stinsert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace signup.Models
+{
+    public class stinsert
+    {
+        public string name { get; set; }
+        public string uname { get; set; }
+        public string pwd { get; set; }
+        public string cpwd { get; set; }
+
+        public string msg { get; set; }
+    }
+}
